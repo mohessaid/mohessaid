d7971a249f702bdd78922cbdfadba22cc6857dda
Mohammed Essaid MEZERREG (mohessaid) <me@mohessaid.com>
2025-02-23 12:11:43 +0100

    👷 Update the build script

--- build.sh
+++ build.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
+set -e
 
 # Fetch the latest Go package URL
 GO_URL=$(curl -s https://go.dev/dl/ | grep -oP '/dl/go[0-9.]*.linux-amd64.tar.gz' | head -n 1)
 
 # Download the latest Go package and rename it to go.tar.gz
-curl -o go.tar.gz https://go.dev$GO_URL
+curl -L -o go.tar.gz https://go.dev$GO_URL
 
-# Extract the package
-tar -C /usr/local -xzf go.tar.gz
+# Extract the package to $HOME
+tar -C $HOME -xzf go.tar.gz
 
-# Set up the environment
-export PATH=$PATH:/usr/local/go/bin
+# Set up the environment for Go
+export PATH=$HOME/go/bin:$PATH
 
 # Verify the Go installation
-go env GOPATH
+go version
 
-# Fetch the latest version of DART SASS.
-export DART_SASS_VERSION=$(curl -sI -L -o /dev/null -w '%{url_effective}' https://github.com/sass/dart-sass/releases/latest | awk -F '/' '{print $NF}')
+# Fetch the latest version of Dart Sass
+DART_SASS_VERSION=$(curl -sI -L -o /dev/null -w '%{url_effective}' https://github.com/sass/dart-sass/releases/latest | awk -F '/' '{print $NF}')
 
-# Fetch the latest version of DART SASS.
-curl -LJO https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz
+# Download Dart Sass
+curl -L -o dart-sass.tar.gz https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz
 
-# Extract the package
-tar -xf dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz -C /usr/local/bin
+# Extract the package to $HOME/bin
+mkdir -p $HOME/bin
+tar -xzf dart-sass.tar.gz -C $HOME/bin --strip-components=1
+
+# Set up the environment for Dart Sass
+export PATH=$HOME/bin:$PATH
+
+# Verify the Dart Sass installation
+sass --version
 
 # Build website
 hugo --gc --minify
